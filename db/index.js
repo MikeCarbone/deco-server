020236c4d376feaeca22ca78d2b5606face9e0f9
Mike Carbone <mfcbone2@gmail.com>
2023-12-19 10:19:03 -0500

    fix(database): adds support for connection via URL (#14)

--- db/index.js
+++ db/index.js
@@ -1,3 +1,8 @@
+/**
+ * We allow two options for connecting to your database:
+ * 1. One .env variable `DBURL`
+ * 2. All .env variables `PGHOST`, `PGPORT`, `PGDATABASE`, `PGUSER`, `PGPASSWORD`
+ */
 import pg from "pg";
 import { config } from "dotenv";
 
@@ -7,21 +12,27 @@ config();
 // Ensure necessary variables are present in a .env file
 const REQUIRED_DB_VARS = ["PGHOST", "PGPORT", "PGDATABASE", "PGUSER", "PGPASSWORD"];
 REQUIRED_DB_VARS.forEach((variable) => {
+  if (!!process.env.DBURL) return;
   if (!process.env[variable]) {
     console.log(`Missing required .env variable ${variable}.`);
     process.exit(1);
   }
 });
 
+const hasConnectionString = !!process.env.DBURL;
+const connection = hasConnectionString
+  ? { connectionString: process.env.DBURL }
+  : {
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      database: process.env.PGDATABASE,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+    };
+
 // Create a pool of connections
 const { Pool } = pg;
-const client = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-});
+const client = new Pool(connection);
 
 // Initiate root database connection
 try {
